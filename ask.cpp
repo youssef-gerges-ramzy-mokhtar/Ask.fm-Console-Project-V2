438a62f61efd3b33d7745b28b02a0f3c3079c199
youssef-gerges-ramzy-mokhtar <youssef.gerges1@outlook.com>
2023-08-06 12:47:18 +0300

    Initial commit

---
+++ ask.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Ask {
+	Ask() {	
+		cout << "Ask.fm Application V2\n";
+	}
+};
+
+int main() {
+	cout << boolalpha;
+	Ask ask = Ask();
+
+	return 0;
+}
